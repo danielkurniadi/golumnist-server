4df5c71f4f24e32fdf70b2d089c487d8d0809f7b
DanielKurniadi <daniel.kurniadi@go-jek.com>
2020-05-30 14:30:17 +0800

    [User] added Followers relationship in MySQL UserDB - create many2many user to user gorm tag - create followership jointable

--- user/repository/mysql/mysql_user.go
+++ user/repository/mysql/mysql_user.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	// import built-in libraries
+	"fmt"
 	"reflect"
 	"regexp"
 	"strings"
@@ -10,8 +11,9 @@ import (
 	// import third-party libraries
 	"github.com/jinzhu/gorm"
 
-	// import our local package
+	// import our local packages
 	"github.com/iqdf/golumn-story-service/domain"
+	repocommon "github.com/iqdf/golumn-story-service/lib/repository"
 )
 
 // DefaultLimit ...
@@ -19,36 +21,42 @@ const DefaultLimit uint = 20
 
 // UserDB ...
 type UserDB struct {
-	ID            uint64 `gorm:"primary_key"`
-	Email         string `gorm:"Type:VARCHAR(15);UNIQUE_INDEX;NOT_NULL"`
-	Username      string `gorm:"Type:VARCHAR(15);UNIQUE_INDEX;NOT NULL"`
-	Name          string `gorm:"Type:VARCHAR(15);INDEX;NOT NULL"`
-	ProfileImgURL string `gorm:"Type:VARCHAR(20);DEFAULT:'/icon/defaultauthor'"`
-	Location      string `gorm:"Type:VARCHAR(10);DEFAULT:'Worldwide'"`
-	Description   string `gorm:"Type:VARCHAR(128);DEFAULT:'The author tend to keep air of mystery of him/herself'"`
-	Followers     int
-	Following     int
-	TwitterName   string `gorm:"Type:VARCHAR(20)"`
-	FacebookName  string `gorm:"Type:VARCHAR(20)"`
-	CreatedAt     time.Time
-	UpdatedAt     time.Time
+	ID             uint64    `gorm:"PRIMARY_KEY"`
+	Email          string    `gorm:"Type:VARCHAR(40);UNIQUE_INDEX;NOT NULL"`
+	Username       string    `gorm:"Type:VARCHAR(40);UNIQUE_INDEX;NOT NULL"`
+	Name           string    `gorm:"Type:VARCHAR(40);INDEX;NOT NULL"`
+	ProfileImgURL  string    `gorm:"Type:VARCHAR(128);DEFAULT:'/icon/defaultpic'"`
+	Location       string    `gorm:"Type:VARCHAR(40);DEFAULT:'Worldwide'"`
+	Description    string    `gorm:"Type:VARCHAR(256);DEFAULT:'The author tend to keep air of mystery of him/herself'"`
+	Followers      []*UserDB `gorm:"MANY2MANY:followership;JOINTABLE_FOREIGNKEY:follower_id;ASSOCIATION_JOINTABLE_FOREIGNKEY:followed_id"`
+	FollowersCount int
+	FollowingCount int
+	TwitterName    string `gorm:"Type:VARCHAR(20)"`
+	FacebookName   string `gorm:"Type:VARCHAR(20)"`
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
 }
 
 // NewUserDBWriter ...
 func NewUserDBWriter(user domain.User) UserDB {
+	// TODO: check NOT NULL string ''
+	// TODO: check minimum length contraint
+	// email: RFC 3696 - Section 3
+	// username: 5 - 40
+	// name: 2 - 40
 	return UserDB{
-		Email:         user.Email,
-		Username:      user.Username,
-		Name:          user.Name,
-		ProfileImgURL: user.ProfileImgURL,
-		Location:      user.Location,
-		Description:   user.Description,
-		Followers:     0,
-		Following:     0,
-		TwitterName:   "",
-		FacebookName:  "",
-		CreatedAt:     time.Now(),
-		UpdatedAt:     time.Now(),
+		Email:          user.Email,
+		Username:       user.Username,
+		Name:           user.Name,
+		ProfileImgURL:  user.ProfileImgURL,
+		Location:       user.Location,
+		Description:    user.Description,
+		FollowersCount: 0,
+		FollowingCount: 0,
+		TwitterName:    "",
+		FacebookName:   "",
+		CreatedAt:      time.Now(),
+		UpdatedAt:      time.Now(),
 	}
 }
 
@@ -80,7 +88,14 @@ func UserColumns() []string {
 
 	columns := make([]string, 0, val.NumField())
 	for i := 0; i < val.NumField(); i++ {
-		column := ToSnakeCase(val.Type().Field(i).Name)
+		field := val.Type().Field(i)
+
+		if strings.Contains(string(field.Tag), "MANY2MANY") {
+			// skip many2many fields as it will not
+			// appear as table column
+			continue
+		}
+		column := ToSnakeCase(field.Name)
 		columns = append(columns, column)
 	}
 	return columns
@@ -94,38 +109,44 @@ func (userDB *UserDB) TableName() string {
 // User ...
 func (userDB *UserDB) User() domain.User {
 	return domain.User{
-		ID:            userDB.ID,
-		Email:         userDB.Email,
-		Username:      userDB.Username,
-		Name:          userDB.Name,
-		ProfileImgURL: userDB.ProfileImgURL,
-		Location:      userDB.Location,
-		Description:   userDB.Description,
-		Followers:     userDB.Followers,
-		Following:     userDB.Following,
-		TwitterName:   userDB.TwitterName,
-		FacebookName:  userDB.FacebookName,
+		ID:             userDB.ID,
+		Email:          userDB.Email,
+		Username:       userDB.Username,
+		Name:           userDB.Name,
+		ProfileImgURL:  userDB.ProfileImgURL,
+		Location:       userDB.Location,
+		Description:    userDB.Description,
+		FollowersCount: userDB.FollowersCount,
+		FollowingCount: userDB.FollowingCount,
+		TwitterName:    userDB.TwitterName,
+		FacebookName:   userDB.FacebookName,
 	}
 }
 
 // UIntRandomizer ...
-// TODO: put under common
 type UIntRandomizer interface {
 	Uint32() uint32
 	Uint64() uint64
 }
 
+// DBErrorConverter ...
+type DBErrorConverter interface {
+	AppError(error, string) error
+}
+
 // UserMySQLRepository ...
 type UserMySQLRepository struct {
-	DB   *gorm.DB
-	Rand UIntRandomizer
+	DB     *gorm.DB
+	Rand   UIntRandomizer
+	ErrCvt DBErrorConverter
 }
 
 // NewUserMySQLRepository ...
 func NewUserMySQLRepository(db *gorm.DB, rand UIntRandomizer) *UserMySQLRepository {
 	return &UserMySQLRepository{
-		DB:   db,
-		Rand: rand,
+		DB:     db,
+		Rand:   rand,
+		ErrCvt: repocommon.NewMySQLErrCvt(),
 	}
 }
 
@@ -140,19 +161,24 @@ func (userRepo *UserMySQLRepository) GetByID(userID uint64) (domain.User, error)
 		db     = userRepo.DB
 	)
 	// SELECT * FROM `users` WHERE (id = ?) ORDER BY `users`.`id` LIMIT 1
-	db = db.Where("id = ?", userID).First(&userDB)
-	return userDB.User(), db.Error
+	err := db.Where("id = ?", userID).First(&userDB).Error
+	appErr := userRepo.ErrCvt.AppError(err, "userrepo: find user by id fail")
+
+	return userDB.User(), appErr
 }
 
 // GetByEmail ...
 func (userRepo *UserMySQLRepository) GetByEmail(email string) (domain.User, error) {
 	var (
-		userDB = new(UserDB)
+		userDB = UserDB{Email: email}
 		db     = userRepo.DB
 	)
 	// SELECT * FROM `users` WHERE (email = ?) ORDER BY `users`.`id` LIMIT 1
-	db = db.Where("email = ?", email).First(&userDB)
-	return userDB.User(), db.Error
+	err := db.Where("email = ?", email).First(&userDB).Error
+	fmt.Println("GetByEmail err", err, "Gorm cannot call Error()")
+	appErr := userRepo.ErrCvt.AppError(err, "userrepo: find user by email fail")
+	fmt.Println("GetByEmail AppErr", appErr)
+	return userDB.User(), appErr
 }
 
 // GetByUsername ...
@@ -162,8 +188,10 @@ func (userRepo *UserMySQLRepository) GetByUsername(username string) (domain.User
 		db     = userRepo.DB
 	)
 	// SELECT * FROM `users` WHERE (username = ?) ORDER BY `users`.`id` LIMIT 1
-	db = db.Where("username = ?", username).First(&userDB)
-	return userDB.User(), db.Error
+	err := db.Where("username = ?", username).First(&userDB).Error
+	appErr := userRepo.ErrCvt.AppError(err, "userrepo: find user by username fail")
+
+	return userDB.User(), appErr
 }
 
 // InsertOne ...
@@ -177,7 +205,8 @@ func (userRepo *UserMySQLRepository) InsertOne(user domain.User) (domain.User, e
 	// INSERT INTO `users` (...) VALUES (...)
 	db = db.Create(&userDB)
 	if err := db.Error; err != nil || db.RowsAffected == 0 {
-		return domain.User{}, err
+		appErr := userRepo.ErrCvt.AppError(err, "userrepo: insert one user fail")
+		return domain.User{}, appErr
 	}
 	return userDB.User(), nil
 }
@@ -191,10 +220,13 @@ func (userRepo *UserMySQLRepository) UpdateOne(userID uint64, user domain.User) 
 
 	// UPDATE `users` SET location = (location), description = (description)
 	// WHERE id = (userID)
-	db = db.Model(&userDB).Updates(userDB) // update attributes of a user row
+	db = db.Set("gorm:association_save_reference", false)
+	db = db.Model(&UserDB{ID: userID}).Updates(userDB) // update attributes of a user row
 	if err := db.Error; err != nil || db.RowsAffected == 0 {
-		return domain.User{}, err
+		appErr := userRepo.ErrCvt.AppError(err, "userrepo: update one user fail")
+		return domain.User{}, appErr
 	}
+
 	return userDB.User(), nil
 }
 
@@ -208,7 +240,8 @@ func (userRepo *UserMySQLRepository) DeleteOne(userID uint64) error {
 	// DELETE FROM `users` WHERE id = ?
 	db = db.Delete(&userDB)
 	if err := db.Error; err != nil || db.RowsAffected == 0 { // do you need to check?
-		return err
+		appErr := userRepo.ErrCvt.AppError(err, "userrepo: delete one user fail")
+		return appErr
 	}
 	return nil
 }
